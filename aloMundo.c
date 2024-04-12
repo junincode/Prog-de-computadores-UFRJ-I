6ef7cfe566755bfc7bdfe98a76f8b86271c982d4
junincode <136363991+junincode@users.noreply.github.com>
2024-04-12 16:27:36 -0300

    Add files via upload

---
+++ aloMundo.c
@@ -0,0 +1,16 @@
+/*
+Descrição: exibe mensagem na tela
+Saída: mensagem padrão exibida na tela
+Defesa: não se aplica (não há restrições de entrada)
+*/
+
+#include <stdio.h>
+
+int main() {
+	printf("\n");
+	printf("Alo Mundo!\n");
+	printf("\n");
+	return 0;
+}
+
+
