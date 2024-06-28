5339eaa4447860297ce262d5eabafdd47d524db8
M.A.W <136363991+junincode@users.noreply.github.com>
2024-06-28 15:08:14 -0300

    Update merge_ordenado.c

--- lab9/merge_ordenado.c
+++ lab9/merge_ordenado.c
@@ -1,41 +1,26 @@
 #include <stdio.h>
 
-/*  A lógica por trás do código não foi inteiramente desenvolvida por mim. Porém, o script abaixo
-    funciona perfeitamente para ordenar uma lista que seja um "merge" de duas listas já ordenadas (assumidas).*/
-int main(void){
-    
-    int n1=0, n2=0, cn1=0, cn2=0, i=0;
-    
-    printf("Tamanho de l1[] e l2[] : ");
-    scanf("%d %d", &n1, &n2);
-    
-    int l1[n1], l2[n2], lordem[n1 + n2];
-    
-    for (int i = 0; i < n1; i++){
-        printf("l[%d] : ", i);
-        scanf("%d", &l1[i]);
+int main(void)
+{
+    int n;
+    printf("Digite um n.o : ");
+    scanf("%d", &n);
+    int v[n];
+    for(int i = 0; i < n; i++){
+        printf("Digite o valor da posição v[%d]: ", i);
+        scanf("%d", &v[i]);
     }
-    
-    for (int i = 0; i < n2; i++){
-        printf("l[%d] : ", i);
-        scanf("%d", &l2[i]);
-    }
-    
-    while (i < n1 + n2) {
-        if (cn1 < n1 && (cn2 >= n2 || l1[cn1] <= l2[cn2])) {
-            lordem[i] = l1[cn1];
-            printf("Escolhi %d\n", l1[cn1]);
-            cn1 += 1;
-        } else if (cn2 < n2 && (cn1 >= n1 || l1[cn1] > l2[cn2])) {
-            lordem[i] = l2[cn2];
-            printf("Escolhi %d\n", l2[cn2]);
-            cn2 += 1;
+    for(int i = 0; i < n; i++){
+        for(int k = 0; k < n - i - 1; k++){
+            if(v[k] > v[k+1]){
+                 v[k] ^= v[k+1];
+                 v[k+1] ^= v[k];
+                 v[k] ^= v[k+1];
+            }
         }
-        i += 1;
     }
-    printf("Lista: ");
-    for(int i = 0; i < n1+n2; i++){
-        printf("%d ", lordem[i]);
+    for(int i = 0; i < n; i++){
+        printf("%d ", v[i]);
     }
     return 0;
 }
